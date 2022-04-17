bda401586631aa28d832766a66fe21a254fd6c27
zethika <zethika594@gmail.com>
2022-04-17 19:12:37 +0200

    Feature: Added pinia

---
+++ src/stores/controls.ts
@@ -0,0 +1,11 @@
+import {defineStore} from "pinia";
+
+export const useControlsStore = defineStore({
+    id: 'controls',
+    state: () => ({
+    }),
+    getters: {
+    },
+    actions: {
+    }
+})
