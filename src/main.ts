bf35dbee9ad0c5c04db9c56a506cec817613baa0
zethika <zethika594@gmail.com>
2022-04-17 20:13:00 +0200

    Feature: Added Primevue

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,11 @@ import {createApp} from 'vue'
 import App from './App.vue'
 import './index.css'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config';
+import 'primevue/resources/themes/lara-dark-purple/theme.css'
+import 'primevue/resources/primevue.min.css'
 
 createApp(App)
+    .use(PrimeVue)
     .use(createPinia())
     .mount('#app')
