51062b94767b73a1d6598777a05af839d35a6eac
zethika <zethika594@gmail.com>
2022-04-18 10:44:30 +0200

    Feature: Added Wopipo colors

--- src/App.vue
+++ src/App.vue
@@ -13,4 +13,37 @@ import Controls from "@/components/structural/Controls.vue";
 
 <style>
 
+:root{
+    /* Colors */
+    --bg: #f4f8f9;
+
+    --blue-lightest: #5686EA;
+    --blue-lighter: #457AE8;
+    --blue: #316ae0;
+    --blue-darker: #2D63D1;
+    --blue-darkest: #295ABE;
+
+    --grey-lighterest: #b0b0b0;
+    --grey-lightest: #AAAAAA;
+    --grey-light-dark: #707070;
+    --grey-lighter: #777777;
+    --grey: #444444;
+    --grey-darker: #222222;
+
+    --green-lightest: #33EB3F;
+    --green-lighter: #24E031;
+    --green: #15D422;
+    --green-darker: #14C720;
+    --green-darkest: #12BA1E;
+
+    --red-lightest: #ff3351;
+    --red-lighter: #f5314f;
+    --red: #E63250;
+
+    --black-20p: rgba(0, 0, 0, 0.2);
+
+    /* Sizes */
+    --header-height: 55px;
+}
+
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,45 @@ module.exports = {
         './src/App.vue',
     ],
     theme: {
+        screens: {
+            'xs': '475px',
+            ...screens,
+        },
         extend: {
+            spacing: {
+                'header': '55px'
+            },
             colors: {
+                blue: {
+                    bg: "var(--bg)",
+                    DEFAULT: "var(--blue)",
+                    lighter: "var(--blue-lighter)",
+                    lightest: "var(--blue-lightest)",
+                    darker: "var(--blue-darker)",
+                    darkest: "var(--blue-darkest)",
+                },
+                grey: {
+                    DEFAULT: "var(--grey)",
+                    lightest: "var(--grey-lightest)",
+                    lighter: "var(--grey-lighter)",
+                    darker: "var(--grey-darker)",
+                },
+                green: {
+                    DEFAULT: "var(--green)",
+                    lighter: "var(--green-lighter)",
+                    lightest: "var(--green-lightest)",
+                    darker: "var(--green-darker)",
+                    darkest: "var(--green-darkest)",
+                },
+                red: {
+                    DEFAULT: "var(--red)",
+                    lightest: "var(--red-lightest)",
+                    lighter: "var(--red-lighter)",
+                },
+                black: {
+                    DEFAULT: 'black',
+                    '20p': 'var(--black-20p)'
+                },
                 transparent: "transparent"
             },
             height: {
