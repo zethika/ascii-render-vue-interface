584e283ce117150eaa9f3a1ea96f4793f64112d0
zethika <zethika594@gmail.com>
2022-04-18 11:15:03 +0200

    Feature: Added data section under controls

---
+++ src/components/controls/DataSection.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import {useControlsStore} from "@/stores/controls";
+import SubTitle from "@/components/text/SubTitle.vue";
+import RegularInput from "@/components/inputs/RegularInput.vue";
+
+const controlsStore = useControlsStore();
+
+</script>
+
+<template>
+    <div>
+        <SubTitle>Data</SubTitle>
+        <div class="grid gap-2">
+            <RegularInput v-model="controlsStore.width" label="Width" type="number" />
+            <RegularInput v-model="controlsStore.height" label="Height" type="number" />
+            <RegularInput v-model="controlsStore.lineHeight" label="Line height" type="number" />
+        </div>
+    </div>
+</template>
--- src/components/structural/Controls.vue
+++ src/components/structural/Controls.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
 import SubTitle from "@/components/text/SubTitle.vue";
 import SourceSection from "@/components/controls/SourceSection.vue";
+import DataSection from "@/components/controls/DataSection.vue";
 </script>
 
 <template>
     <div class="w-[300px] bg-gray-200 p-2">
         <SourceSection class="border-b border-blue-600 pb-4 mb-3" />
-        <SubTitle>Data</SubTitle>
+        <DataSection class="border-b border-blue-600 pb-4 mb-3" />
         <SubTitle>Colors</SubTitle>
     </div>
 </template>
