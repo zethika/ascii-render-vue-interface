bb78c819333b2c49674696bed3402cccd34280ff
zethika <zethika594@gmail.com>
2022-04-18 10:38:55 +0200

    Fix: Made app take up full height

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 import Renderer from "@/components/Renderer.vue";
 import Controls from "@/components/structural/Controls.vue";
+
 </script>
 
 <template>
     <div class="flex flex-row h-screen w-screen">
-        <Renderer class="flex-grow"/>
+        <Renderer class="flex-grow h-full"/>
         <Controls />
     </div>
 </template>
