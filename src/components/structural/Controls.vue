26cdc7a777de77190344fee8e4cdbf82ff7ca80c
zethika <zethika594@gmail.com>
2022-04-18 08:03:20 +0200

    Feature: Removed unused elements

--- src/components/structural/Controls.vue
+++ src/components/structural/Controls.vue
@@ -1,12 +1,6 @@
 <script setup lang="ts">
-import Slider from 'primevue/slider';
-import InputText from 'primevue/inputtext';
-import {ref} from "vue";
 import SubTitle from "@/components/text/SubTitle.vue";
 import SourceSection from "@/components/controls/SourceSection.vue";
-
-const value = ref(0)
-
 </script>
 
 <template>
@@ -14,12 +8,5 @@ const value = ref(0)
         <SourceSection class="border-b border-blue-600 pb-4 mb-3" />
         <SubTitle>Data</SubTitle>
         <SubTitle>Colors</SubTitle>
-
-<!--        <Slider v-model="value" />
-        <span class="p-input-icon-left">
-            <i class="pi pi-search" />
-            <InputText type="text" v-model="value" placeholder="Search" />
-        </span>-->
-
     </div>
 </template>
