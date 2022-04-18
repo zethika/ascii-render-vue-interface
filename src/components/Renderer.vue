80b6c1402fb888bc473bf4f5fbe1558edd818466
zethika <zethika594@gmail.com>
2022-04-18 13:31:27 +0200

    Feature: Displayed VideoRenderer on webcam mode

--- src/components/Renderer.vue
+++ src/components/Renderer.vue
@@ -2,12 +2,14 @@
 import {useControlsStore} from "@/stores/controls";
 import ImageRenderer from "@/components/renderers/ImageRenderer.vue";
 import CenteredElement from "@/components/structural/CenteredElement.vue";
+import VideoRenderer from "@/components/renderers/VideoRenderer.vue";
 
 const controlStore = useControlsStore();
 </script>
 
 <template>
-    <CenteredElement v-if="controlStore.file === null" class="h-full"><p >Select a source to the right</p></CenteredElement>
+    <VideoRenderer v-if="controlStore.useWebcam === true" />
+    <CenteredElement v-else-if="controlStore.file === null" class="h-full"><p >Select a source to the right</p></CenteredElement>
     <ImageRenderer v-else-if="controlStore.file.type.indexOf('image/') !== -1" />
     <CenteredElement v-else class="h-full"><p >No renderer found for file type "{{controlStore.file.type}}"</p></CenteredElement>
 </template>
