5b02177466734c5a58739a78372f7f498f58bf55
zethika <zethika594@gmail.com>
2022-04-18 08:04:55 +0200

    Refactor: Moved file storage out to store

--- src/components/controls/SourceSection.vue
+++ src/components/controls/SourceSection.vue
@@ -1,12 +1,10 @@
 <script setup lang="ts">
 import SubTitle from "@/components/text/SubTitle.vue";
 import FileUpload from 'primevue/fileupload';
-import {reactive} from "vue";
 import InputFilePreview from "@/components/image/InputFilePreview.vue";
+import {useControlsStore} from "@/stores/controls";
 
-const state = reactive({
-    file: null as File|null,
-})
+const controlStore = useControlsStore()
 
 function select(e: any)
 {
@@ -14,7 +12,7 @@ function select(e: any)
     if(typeof e.files !== 'undefined' && typeof e.files[0] !== 'undefined' && e.files[0] instanceof File)
         file = e.files[0];
 
-    state.file = file;
+    controlStore.file = file;
 }
 
 </script>
@@ -23,6 +21,6 @@ function select(e: any)
     <div>
         <SubTitle>Source</SubTitle>
         <FileUpload mode="basic" name="demo[]"  accept="image/*" :maxFileSize="100000000" @upload="() => null" @select="select" />
-        <InputFilePreview v-if="state.file !== null" :file="state.file" class="max-w-full mt-4" />
+        <InputFilePreview v-if="controlStore.file !== null" :file="controlStore.file" class="max-w-full mt-4" />
     </div>
 </template>
--- src/stores/controls.ts
+++ src/stores/controls.ts
@@ -3,6 +3,7 @@ import {defineStore} from "pinia";
 export const useControlsStore = defineStore({
     id: 'controls',
     state: () => ({
+        file: null as null|File
     }),
     getters: {
     },
