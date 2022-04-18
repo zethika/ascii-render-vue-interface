d1f809965052b837f1809bbd119eb4ead28850d8
zethika <zethika594@gmail.com>
2022-04-18 10:58:44 +0200

    Refactor: Removed Primevue

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,7 @@ import {createApp} from 'vue'
 import App from './App.vue'
 import './index.css'
 import { createPinia } from 'pinia'
-import PrimeVue from 'primevue/config';
-import 'primevue/resources/themes/lara-light-blue/theme.css'
-import 'primevue/resources/primevue.min.css'
-import 'primeicons/primeicons.css'
 
 createApp(App)
-    .use(PrimeVue)
     .use(createPinia())
     .mount('#app')
