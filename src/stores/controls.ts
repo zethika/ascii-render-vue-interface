267f25a2b5d20d18a20960d84d7b1a9cd8cdb3a2
zethika <zethika594@gmail.com>
2022-04-18 10:10:40 +0200

    Feature: Added characterLength getter

--- src/stores/controls.ts
+++ src/stores/controls.ts
@@ -3,9 +3,13 @@ import {defineStore} from "pinia";
 export const useControlsStore = defineStore({
     id: 'controls',
     state: () => ({
-        file: null as null|File
+        file: null as null|File,
+        width: 10,
+        height: 10,
+        characters: 'Ñ@#W$9876543210?!abc;:+=-,._ '
     }),
     getters: {
+        charactersLength: (state) => state.characters.length
     },
     actions: {
     }
