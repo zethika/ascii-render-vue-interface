dd224d6e9e43111dcdd1aa64a99eef61960d8088
zethika <zethika594@gmail.com>
2022-04-18 09:28:59 +0200

    Feature: Added PixelMediaCanvas class

---
+++ src/classes/PixelMediaCanvas.ts
@@ -0,0 +1,55 @@
+
+export default class PixelMediaCanvas{
+    private _canvas: HTMLCanvasElement;
+    private _image: HTMLImageElement|null = null;
+    private _pixelsArray: Array<number>|null = null;
+    private _width: number;
+    private _height: number;
+
+    constructor(canvas: HTMLCanvasElement,width: number, height: number) {
+        this._canvas = canvas;
+        this._width = width;
+        this._height = height;
+    }
+
+    set canvas(value: HTMLCanvasElement) {
+        this._canvas = value;
+    }
+
+    get pixelsArray(): Array<number> | null {
+        return this._pixelsArray;
+    }
+
+    get width(): number {
+        return this._width;
+    }
+
+    set width(value: number) {
+        this._width = value;
+    }
+
+    get height(): number {
+        return this._height;
+    }
+
+    set height(value: number) {
+        this._height = value;
+    }
+
+    private getContext(): CanvasRenderingContext2D{
+        return this.canvas.getContext('2d') as CanvasRenderingContext2D;
+    }
+
+    public loadImageFromFile(file: File)
+    {
+        this._image = new Image;
+        this._image.onload = () => {
+            if(this._image !== null)
+            {
+                this.getContext().drawImage(this._image, 20,20);
+                URL.revokeObjectURL(this._image.src)
+            }
+        }
+        this._image.src = URL.createObjectURL(file);
+    }
+}
