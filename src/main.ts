0af3ddee6cba1d9ce816c68ccb09bc1f59f4feea
zethika <zethika594@gmail.com>
2022-04-18 08:00:33 +0200

    Feature: Changed theme

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import App from './App.vue'
 import './index.css'
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config';
-import 'primevue/resources/themes/lara-dark-indigo/theme.css'
+import 'primevue/resources/themes/lara-light-blue/theme.css'
 import 'primevue/resources/primevue.min.css'
 import 'primeicons/primeicons.css'
 
